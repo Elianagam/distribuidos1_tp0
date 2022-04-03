b05e82af5a761be7ae420eec2ccdefd526576a07
Eliana Gamarra <elianagam2@gmail.com>
2022-04-02 22:40:28 -0300

    Delete script.sh

--- ejercicio3/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export dest=$(docker inspect distribuidos1_tp0_testing_net |grep  IPv4Address | sed 's/.*IPv4Address": "//'|sed 's/",$//' | sed 's/\/.//');
-
-echo "test" | nc $dest 12345
